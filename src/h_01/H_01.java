b6e46e6ec1dc4e6573f53592b066e2964bf6efd9
tothm23 <tothm.113sz@acsjszki.hu>
2023-03-01 19:49:23 +0100

    Finish

---
+++ src/h_01/H_01.java
@@ -0,0 +1,34 @@
+package h_01;
+
+/**
+ *
+ * @author Tóth Milán
+ */
+public class H_01 {
+
+    public static void main(String[] args) {
+        /*
+        System.out.println("Hello");
+        String szoveg = "Ez egy \"alma\" szöveg";
+        */
+        int szam = 12;
+        System.out.println(szam);
+        /*
+        boolean flag = true;
+        String szoveg2;
+        int szam2;
+        char karakter = 'a';
+        Integer szam3 = 12;
+        
+        Object proba = 10;
+        Integer konvertalt = Integer.parseInt(proba.toString());
+        System.out.println(konvertalt + 8);
+        
+        System.out.println(7 / 3);
+        System.out.println(7 % 3);
+        System.out.println((double) 7 / 3);
+        System.out.println(7 / 3.0);
+        */
+    }
+    
+}
